b8ac95d9d2a0246c4b3b14f3e91423b911b1be3f
Mateusz Grzywacz <amateusz@users.noreply.github.com>
2018-05-01 11:54:49 +0100

    Update zabawy_1.js
    now it's so cryptic. I'll make it properly after work

--- zabawy_1.js
+++ zabawy_1.js
@@ -181,7 +181,9 @@ function get_new_gif(query) {
     new_gif.width = url.data.fixed_width_downsampled_width;
     new_gif.height = url.data.fixed_width_downsampled_height;
     
-    new_gif.preview = loadImage(url.data.images.fixed_height_small_still.url)
+    var pat = url.data.images.fixed_height_small_still.url.split('/')[0];    
+    new_gif.preview = loadImage(url.data.image_url.replace(pat, url.data.images.fixed_height_small_still.url).split('/giphy.gif')[0];)
+    
     new_gif.positionX = mouseX-new_gif.width/2;
     new_gif.positionY = mouseY-new_gif.height/2;
     new_gif.shearX = 0;
