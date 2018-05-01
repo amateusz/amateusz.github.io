3316d6519a47bb4f2e8f80835ae6a394147c248e
Mateusz Grzywacz <amateusz.grzywacz@gmail.com>
2018-05-01 20:22:13 +0100

    fixed mouse-gif intersect function to function (sic) in both directions, thus fixing deletion. duplicated for with different conditions - I know, _crude_

--- zabawy_1.js
+++ zabawy_1.js
@@ -19,22 +19,22 @@ function draw() {
   background(bgColour);
   //background("#A50365");
   for (var i = 0, len = gifs.length; i < len; i++) {
-      // temporarily push away canvas origin
-      push();
-      translate(gifs[i].positionX, gifs[i].positionY); 
-      // transformations
-      rotate(gifs[i].rotation);
-      scale(gifs[i].scaleX, gifs[i].scaleY);
-      shearX(gifs[i].shearX);
-      shearY(gifs[i].shearY);
-      // and blit
-      if (gifs[i].loaded()) {
-        image(gifs[i], -gifs[i].width/2, -gifs[i].height/2); // actual gif
-      }else{
-        image(gifs[i].preview, -gifs[i].preview.width/2, -gifs[i].preview.height/2); // if not ready, then only a mere preview
-      }  
-      // restore canvas origin
-      pop();
+    // temporarily push away canvas origin
+    push();
+    translate(gifs[i].positionX, gifs[i].positionY); 
+    // transformations
+    rotate(gifs[i].rotation);
+    scale(gifs[i].scaleX, gifs[i].scaleY);
+    shearX(gifs[i].shearX);
+    shearY(gifs[i].shearY);
+    // and blit
+    if (gifs[i].loaded()) {
+      image(gifs[i], -gifs[i].width/2, -gifs[i].height/2); // actual gif
+    } else {
+      image(gifs[i].preview, -gifs[i].preview.width/2, -gifs[i].preview.height/2); // if not ready, then only a mere preview
+    }  
+    // restore canvas origin
+    pop();
   }
   //try {
   //  if (gifs[0].loaded()) {
@@ -63,7 +63,7 @@ function keyPressed() {
       // check for collision with gif. it should be method of FloatingGif object, but not for now..
       //gifs.splice(gifs.length-1, 1);
       try {
-        gifs.splice(gif_intersects_with_mouse(), 1);
+        gifs.splice(gif_intersects_with_mouse(true), 1);
       }
       catch (err) {
       }
@@ -102,12 +102,23 @@ function keyReleased() {
   return false;
 }
 
-function gif_intersects_with_mouse() {
-  for (var i = gifs.length - 1; i >= 0; i--) {
-    // check for disjoin
-    if ((abs(mouseX - (gifs[i].positionX + gifs[i].width/2)) > gifs[i].width/2) || (abs(mouseY - (gifs[i].positionY + gifs[i].height/2)) > gifs[i].height/2)) {
-    } else {
-      return i;
+function gif_intersects_with_mouse(reverse) {
+  if (reverse === undefined) reverse = false; // what is wrong with this language ?!
+  if (!reverse) {
+    for (var i = 0; i < gifs.length; i++) {
+      // check for disjoin
+      if ((abs(mouseX - (gifs[i].positionX + gifs[i].width/2)) > gifs[i].width/2) || (abs(mouseY - (gifs[i].positionY + gifs[i].height/2)) > gifs[i].height/2)) {
+      } else {
+        return i;
+      }
+    }
+  } else {
+    for (var i = gifs.length - 1; i >= 0; i--) {
+      // check for disjoin
+      if ((abs(mouseX - (gifs[i].positionX + gifs[i].width/2)) > gifs[i].width/2) || (abs(mouseY - (gifs[i].positionY + gifs[i].height/2)) > gifs[i].height/2)) {
+      } else {
+        return i;
+      }
     }
   }
   return false;
@@ -180,18 +191,18 @@ function get_new_gif(query) {
     //var new_gif = loadGif(url.data.fixed_width_downsampled_url);
     //new_gif.width = url.data.fixed_width_downsampled_width;
     //new_gif.height = url.data.fixed_width_downsampled_height;
-    
+
     var pat = url.data.id; // id only, no filename // images.fixed_height_small_still.url.split('/')[0]
-//     new_gif.preview = loadImage(url.data.image_url
-//                                 .replace(pat, url.data.images.fixed_height_small_still.url)
-//                                 .split('/giphy.gif')[0] 
-//                                 );
-    
+    //     new_gif.preview = loadImage(url.data.image_url
+    //                                 .replace(pat, url.data.images.fixed_height_small_still.url)
+    //                                 .split('/giphy.gif')[0] 
+    //                                 );
+
     new_gif.preview = loadImage(url.data.images.downsized_still.url)
-//     new_gif.preview.width = url.data.images["480w_still"].width;
-//     new_gif.preview.height = url.data.images["480w_still"].height;
-   
-    new_gif.positionX = mouseX; //-new_gif.width/2;
+      //     new_gif.preview.width = url.data.images["480w_still"].width;
+      //     new_gif.preview.height = url.data.images["480w_still"].height;
+
+      new_gif.positionX = mouseX; //-new_gif.width/2;
     new_gif.positionY = mouseY; //-new_gif.height/2;
     new_gif.shearX = 0;
     new_gif.shearY = 0;
